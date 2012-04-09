de40d8402745309b42ac5b73528ea35372eb5c52
John Verzani <jverzani@gmail.com>
2012-04-08 21:15:18 -0400

    make expanding box container wdget

--- R/gexpandgroup.R
+++ R/gexpandgroup.R
@@ -1,6 +1,9 @@
 ##' @include gframe.R
 NULL
 
+## TODO: on hide, can we update geometry of parent to collapse to necessary size?
+
+
 ##' toolkit constructor
 ##'
 ##' @export
@@ -18,34 +21,93 @@ NULL
 GExpandGroup <- setRefClass("GExpandGroup",
                             contains="GBoxContainer",
                             fields=list(
+                              trigger="ANY",
+                              label="ANY",
+                              inner="ANY",
+                              is_open="logical",
                               "markup"="logical"
                               ),
                             methods=list(
-                              initialize=function(toolkit=NULL, text, markup=FALSE, horizontal=TRUE, handler, action, container=NULL, ..., expand=FALSE, fill=FALSE) {
-
-                                ## XXX
-                                stop("Need to implement this -- but there is no nice widget!")
+                              initialize=function(toolkit=NULL, text="", markup=FALSE, horizontal=TRUE, handler, action, container=NULL, ..., expand=TRUE, fill=TRUE) {
 
                                 
+                                widget <<- make_widget(horizontal)
+                                block <<- make_block()
+                                qconnect(trigger, "clicked", function() {
+                                  .self$toggle_visible()
+                                })
+
+                                initFields(change_signal="toggled", # none really
+                                           is_open=TRUE,
+                                           markup=markup # ignored
+                                           )
+                                set_names(text)
+                                set_visible(TRUE)
+
                                 handler_id <<- add_handler_changed(handler, action)
                                 add_to_parent(container, .self, expand, fill, ...)
-                                
+
                                 callSuper(toolkit, horizontal=horizontal, ...)
                               },
+                              make_block=function() {
+                                "Make enclosing block"
+                                ## Now put into block (widget is a layout)
+                                ## Be careful to explicitly parent those objects not stored in the environment
+                                align <- Qt$Qt$AlignLeft & Qt$Qt$AlignTop
+                                block <<- Qt$QWidget()
+                                
+                                lyt <- Qt$QGridLayout(block) # add parent
+                                block$setLayout(lyt)
+                                
+                                trigger <<- Qt$QPushButton()
+                                label <<- Qt$QLabel("")
+                                inner <<- Qt$QWidget()
+
+                                inner$setLayout(widget)
+                                
+                                lyt$addWidget(trigger, 0, 0, align)
+                                lyt$addWidget(label, 0, 1, align)
+                                lyt$addWidget(inner, 1, 0, 1, 2, align)
+                                
+                                ## don't stretch button
+                                lyt$setRowStretch(0, 0L)
+                                lyt$setRowStretch(1, 1L)
+                                lyt$setColumnStretch(0, 0L)
+                                lyt$setColumnStretch(1, 1L)
+
+                                block
+                              },
+                              get_visible=function() {
+                                is_open
+                              },
+                              set_visible=function(value) {
+                                if(missing(value))
+                                  value <- !is_open # toggle
+
+                                if(value) {
+                                  inner$show()
+                                  trigger$setIcon(as_qicon(Qt$QStyle$SP_TitleBarShadeButton))
+                                } else {
+                                  inner$hide()
+                                  trigger$setIcon(as_qicon(Qt$QStyle$SP_TitleBarUnshadeButton))
+                                  block$update()
+                                  block$updateGeometry()
+                                }
+                                ## update geometry?
+                                block
+                                is_open <<- value
+                                invoke_change_handler()
+                              },
+                              toggle_visible=function() set_visible(!is_open),
                               get_names=function(...) {
-                                block$getLabel()
+                                label$text
                               },
                               set_names=function(value, ...) {
-                                block$setLabel(value)
+                                if(is.null(value))
+                                  value <- ""
+                                label$setText(value)
                               },
-                              get_visible = function() {
-                                block$getExpanded()
-                              },
-                              set_visible = function(value) {
-                                block$setExpanded(as.logical(value))
-                              },
-                              add_handler_changed=function(handler, action, ...) {
-                                add_handler("activate", handler, action, ...)
-                              }
+                              ## override this
+                              connect_to_toolkit_signal=function(...) {}
                               ))
                             
