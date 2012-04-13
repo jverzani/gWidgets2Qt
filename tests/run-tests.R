ff1095598ec9a74e697755e10dcf1acfcb64a5b8
John Verzani <jverzani@gmail.com>
2012-04-13 15:28:25 -0400

    add unit tests

---
+++ tests/run-tests.R
@@ -0,0 +1,12 @@
+library(testthat)
+library(gWidgets2)
+options(guiToolkit="Qt")
+
+f <- list.files(system.file("tests", package="gWidgets2"), full=T)
+f <- Filter(function(x) !grepl("README", x), f)
+f <- Filter(function(x) !grepl("combobox", x), f)
+
+sapply(f, function(i) {
+  message("testing ", i)
+  source(i)
+})
